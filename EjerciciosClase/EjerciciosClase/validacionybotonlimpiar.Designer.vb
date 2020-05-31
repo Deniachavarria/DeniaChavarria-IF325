36d9b475cd65d59657a812d778f55321482ddf6a
Denia Chavarria <julissachavarria884@gmail.com>
2020-05-30 22:47:43 -0500

    Primeras validaciones y boton limpiar

---
+++ EjerciciosClase/EjerciciosClase/validacionybotonlimpiar.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class validacionybotonlimpiar
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtnumero = New System.Windows.Forms.TextBox()
+        Me.btnvalidar = New System.Windows.Forms.Button()
+        Me.btnlimpiar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(36, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Numero"
+        '
+        'txtnumero
+        '
+        Me.txtnumero.Location = New System.Drawing.Point(81, 22)
+        Me.txtnumero.Name = "txtnumero"
+        Me.txtnumero.Size = New System.Drawing.Size(82, 20)
+        Me.txtnumero.TabIndex = 1
+        '
+        'btnvalidar
+        '
+        Me.btnvalidar.Location = New System.Drawing.Point(25, 66)
+        Me.btnvalidar.Name = "btnvalidar"
+        Me.btnvalidar.Size = New System.Drawing.Size(75, 23)
+        Me.btnvalidar.TabIndex = 2
+        Me.btnvalidar.Text = "Validar"
+        Me.btnvalidar.UseVisualStyleBackColor = True
+        '
+        'btnlimpiar
+        '
+        Me.btnlimpiar.Location = New System.Drawing.Point(106, 66)
+        Me.btnlimpiar.Name = "btnlimpiar"
+        Me.btnlimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.btnlimpiar.TabIndex = 3
+        Me.btnlimpiar.Text = "Limpiar"
+        Me.btnlimpiar.UseVisualStyleBackColor = True
+        '
+        'validacionybotonlimpiar
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(222, 127)
+        Me.Controls.Add(Me.btnlimpiar)
+        Me.Controls.Add(Me.btnvalidar)
+        Me.Controls.Add(Me.txtnumero)
+        Me.Controls.Add(Me.Label1)
+        Me.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Name = "validacionybotonlimpiar"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "validacionybotonlimpiar"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtnumero As TextBox
+    Friend WithEvents btnvalidar As Button
+    Friend WithEvents btnlimpiar As Button
+End Class
---
+++ EjerciciosClase/EjerciciosClase/validacionybotonlimpiar.vb
@@ -0,0 +1,27 @@
+﻿Public Class validacionybotonlimpiar
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnlimpiar.Click
+        txtnumero.Clear()
+        txtnumero.Focus()
+
+    End Sub
+
+    Private Sub btnvalidar_Click(sender As Object, e As EventArgs) Handles btnvalidar.Click
+        Dim valor As Integer
+
+        If txtnumero.Text = "" Then
+            MsgBox("Debe ingresar un valor", vbInformation)
+        ElseIf Not IsNumeric(txtnumero.Text) Then
+            MsgBox("Ingrese un valor numerico", vbInformation)
+            txtnumero.Text = ""
+        ElseIf IsNumeric(valor) Then
+            valor = Val(txtnumero.Text)
+            If valor = 0 Then
+                MsgBox("Numero cero", vbInformation)
+            ElseIf valor > 0 Then
+                MsgBox("Numero Positivo")
+            ElseIf valor < 0 Then
+                MsgBox("Numero Negativo")
+            End If
+        End If
+    End Sub
+End Class
