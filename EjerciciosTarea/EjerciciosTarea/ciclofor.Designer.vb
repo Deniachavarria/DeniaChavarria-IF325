cd6a362acf3ac3802ef86515aea54466bfc7dd78
Denia Chavarria <julissachavarria884@gmail.com>
2020-05-31 18:27:53 -0500

    Ciclo for investigacion

---
+++ EjerciciosTarea/EjerciciosTarea/ciclofor.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtnumero = New System.Windows.Forms.TextBox()
+        Me.btncalcular = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnnuevo = New System.Windows.Forms.Button()
+        Me.btnsalir = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.PeachPuff
+        Me.GroupBox1.Controls.Add(Me.txtnumero)
+        Me.GroupBox1.Controls.Add(Me.btncalcular)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.btnnuevo)
+        Me.GroupBox1.Controls.Add(Me.btnsalir)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(250, 138)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Tabla de multiplicar"
+        '
+        'txtnumero
+        '
+        Me.txtnumero.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.txtnumero.Location = New System.Drawing.Point(120, 43)
+        Me.txtnumero.Name = "txtnumero"
+        Me.txtnumero.Size = New System.Drawing.Size(100, 20)
+        Me.txtnumero.TabIndex = 0
+        '
+        'btncalcular
+        '
+        Me.btncalcular.BackColor = System.Drawing.Color.Salmon
+        Me.btncalcular.CausesValidation = False
+        Me.btncalcular.Location = New System.Drawing.Point(6, 90)
+        Me.btncalcular.Name = "btncalcular"
+        Me.btncalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btncalcular.TabIndex = 5
+        Me.btncalcular.Text = "Calcular"
+        Me.btncalcular.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Numero de la tabla"
+        '
+        'btnnuevo
+        '
+        Me.btnnuevo.BackColor = System.Drawing.Color.Salmon
+        Me.btnnuevo.CausesValidation = False
+        Me.btnnuevo.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnnuevo.Location = New System.Drawing.Point(87, 90)
+        Me.btnnuevo.Name = "btnnuevo"
+        Me.btnnuevo.Size = New System.Drawing.Size(75, 23)
+        Me.btnnuevo.TabIndex = 0
+        Me.btnnuevo.Text = "Nuevo"
+        Me.btnnuevo.UseVisualStyleBackColor = False
+        '
+        'btnsalir
+        '
+        Me.btnsalir.BackColor = System.Drawing.Color.Salmon
+        Me.btnsalir.CausesValidation = False
+        Me.btnsalir.Location = New System.Drawing.Point(168, 90)
+        Me.btnsalir.Name = "btnsalir"
+        Me.btnsalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnsalir.TabIndex = 1
+        Me.btnsalir.Text = "Salir"
+        Me.btnsalir.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightSalmon
+        Me.ClientSize = New System.Drawing.Size(271, 161)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Ciclo For"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtnumero As TextBox
+    Friend WithEvents btncalcular As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnnuevo As Button
+    Friend WithEvents btnsalir As Button
+End Class
---
+++ EjerciciosTarea/EjerciciosTarea/ciclofor.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Private Sub btncalcular_Click(sender As Object, e As EventArgs) Handles btncalcular.Click
+        Dim numero As Integer
+        Dim i As Integer
+
+        If txtnumero.Text = "" Then
+            MsgBox("Debe ingresar un valor numerico", vbInformation)
+            txtnumero.Focus()
+        End If
+
+        numero = Val(txtnumero.Text)
+        If txtnumero.Text < 1 Then
+            MsgBox("Debe ingresar un valor mayor a cero", vbInformation)
+            txtnumero.Clear()
+            txtnumero.Focus()
+        ElseIf txtnumero.Text > 0 Then
+            For i = 1 To 10
+                MsgBox(numero & "*" & i & "=" & numero * i)
+            Next i
+        End If
+    End Sub
+
+    Private Sub btnnuevo_Click(sender As Object, e As EventArgs) Handles btnnuevo.Click
+        txtnumero.Clear()
+        txtnumero.Focus()
+    End Sub
+
+    Private Sub btnsalir_Click(sender As Object, e As EventArgs) Handles btnsalir.Click
+        End
+    End Sub
+End Class
