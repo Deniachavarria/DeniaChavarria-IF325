fc5ad6bbcab76bd7db116e6c5d14345d423645f9
Denia Chavarria <julissachavarria884@gmail.com>
2020-05-30 21:46:37 -0500

    Suma de dos valores

---
+++ EjerciciosClase/EjerciciosClase/sumadedosvalorez.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtvalor1 = New System.Windows.Forms.TextBox()
+        Me.txtvalor2 = New System.Windows.Forms.TextBox()
+        Me.txtresultado = New System.Windows.Forms.TextBox()
+        Me.btncalcular = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Valor1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Valor2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(146, 29)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Resultado"
+        '
+        'txtvalor1
+        '
+        Me.txtvalor1.Location = New System.Drawing.Point(55, 26)
+        Me.txtvalor1.Name = "txtvalor1"
+        Me.txtvalor1.Size = New System.Drawing.Size(71, 20)
+        Me.txtvalor1.TabIndex = 3
+        '
+        'txtvalor2
+        '
+        Me.txtvalor2.Location = New System.Drawing.Point(55, 65)
+        Me.txtvalor2.Name = "txtvalor2"
+        Me.txtvalor2.Size = New System.Drawing.Size(71, 20)
+        Me.txtvalor2.TabIndex = 4
+        '
+        'txtresultado
+        '
+        Me.txtresultado.Location = New System.Drawing.Point(207, 26)
+        Me.txtresultado.Name = "txtresultado"
+        Me.txtresultado.Size = New System.Drawing.Size(71, 20)
+        Me.txtresultado.TabIndex = 5
+        '
+        'btncalcular
+        '
+        Me.btncalcular.Location = New System.Drawing.Point(179, 65)
+        Me.btncalcular.Name = "btncalcular"
+        Me.btncalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btncalcular.TabIndex = 6
+        Me.btncalcular.Text = "Calcular"
+        Me.btncalcular.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(298, 121)
+        Me.Controls.Add(Me.btncalcular)
+        Me.Controls.Add(Me.txtresultado)
+        Me.Controls.Add(Me.txtvalor2)
+        Me.Controls.Add(Me.txtvalor1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtvalor1 As TextBox
+    Friend WithEvents txtvalor2 As TextBox
+    Friend WithEvents txtresultado As TextBox
+    Friend WithEvents btncalcular As Button
+End Class
---
+++ EjerciciosClase/EjerciciosClase/sumadedosvalorez.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub btncalcular_Click(sender As Object, e As EventArgs) Handles btncalcular.Click
+        Dim valor1 As Integer
+        Dim valor2 As Integer
+        Dim suma As Integer
+
+        valor1 = Val(txtvalor1.Text)
+        valor2 = Val(txtvalor2.Text)
+
+        If txtvalor1.Text = "" Or txtvalor2.Text = "" Then
+            MsgBox("Debe ingresar un valor", vbInformation)
+        End If
+
+        suma = valor1 + valor2
+
+        txtresultado.Text = suma
+    End Sub
+End Class
