44d42dd719ac86b8483c6d05382965ae6f858aa6
Denia Chavarria <julissachavarria884@gmail.com>
2020-06-06 19:53:44 -0500

    Calcular la formula general cuadratica

---
+++ EjerciciosTareaas/EjerciciosTareaas/formulageneralcuadratica.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class formulageneralcuadratica
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.txta = New System.Windows.Forms.TextBox()
+        Me.txtb = New System.Windows.Forms.TextBox()
+        Me.txtc = New System.Windows.Forms.TextBox()
+        Me.txtx1 = New System.Windows.Forms.TextBox()
+        Me.txtx2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(6, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(23, 21)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "a:"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.GroupBox1.Controls.Add(Me.txtc)
+        Me.GroupBox1.Controls.Add(Me.txtb)
+        Me.GroupBox1.Controls.Add(Me.txta)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(141, 129)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Valores a ingresar"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(6, 93)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(22, 21)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "c:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(6, 62)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(23, 21)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "b:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(10, 73)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(31, 18)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "X2:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(10, 45)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(31, 18)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "X1:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.GroupBox2.Controls.Add(Me.txtx2)
+        Me.GroupBox2.Controls.Add(Me.txtx1)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Location = New System.Drawing.Point(159, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(141, 129)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Ressultados"
+        '
+        'txta
+        '
+        Me.txta.Location = New System.Drawing.Point(49, 35)
+        Me.txta.Name = "txta"
+        Me.txta.Size = New System.Drawing.Size(82, 20)
+        Me.txta.TabIndex = 3
+        '
+        'txtb
+        '
+        Me.txtb.Location = New System.Drawing.Point(49, 65)
+        Me.txtb.Name = "txtb"
+        Me.txtb.Size = New System.Drawing.Size(82, 20)
+        Me.txtb.TabIndex = 4
+        '
+        'txtc
+        '
+        Me.txtc.Location = New System.Drawing.Point(49, 96)
+        Me.txtc.Name = "txtc"
+        Me.txtc.Size = New System.Drawing.Size(82, 20)
+        Me.txtc.TabIndex = 5
+        '
+        'txtx1
+        '
+        Me.txtx1.Location = New System.Drawing.Point(47, 46)
+        Me.txtx1.Name = "txtx1"
+        Me.txtx1.Size = New System.Drawing.Size(80, 20)
+        Me.txtx1.TabIndex = 5
+        '
+        'txtx2
+        '
+        Me.txtx2.Location = New System.Drawing.Point(47, 76)
+        Me.txtx2.Name = "txtx2"
+        Me.txtx2.Size = New System.Drawing.Size(80, 20)
+        Me.txtx2.TabIndex = 6
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button1.Location = New System.Drawing.Point(206, 147)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(54, 23)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "END"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button2.Location = New System.Drawing.Point(146, 147)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(54, 23)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "NEW"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button3.Location = New System.Drawing.Point(54, 147)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(86, 23)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "CALCULATE"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'formulageneralcuadratica
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(314, 177)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "formulageneralcuadratica"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "formulageneralcuadratica"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents txtc As TextBox
+    Friend WithEvents txtb As TextBox
+    Friend WithEvents txta As TextBox
+    Friend WithEvents txtx2 As TextBox
+    Friend WithEvents txtx1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ EjerciciosTareaas/EjerciciosTareaas/formulageneralcuadratica.vb
@@ -0,0 +1,49 @@
+﻿Public Class formulageneralcuadratica
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim a, b, c, d As Double
+        If txta.Text < 1 Then
+            MsgBox(" >a< debe ser diferente de cero", vbInformation)
+            txta.Text = ""
+        Else
+            a = Val(txta.Text)
+            b = Val(txtb.Text)
+            c = Val(txtc.Text)
+
+            d = ((b ^ 2) - 4 * a * c)
+        End If
+
+        If a <> 0 Then
+            If d < 0 Then
+                MsgBox(" No existe una solucion real para la ecuacion", vbInformation)
+                txta.Clear()
+                txtb.Clear()
+                txtc.Clear()
+            Else
+                txtx1.Text = (-b + (d) ^ (1 / 2)) / (2 * a)
+                txtx2.Text = (-b - (d) ^ (1 / 2)) / (2 * a)
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        txta.Clear()
+        txtb.Clear()
+        txtc.Clear()
+        txtx1.Clear()
+        txtx2.Clear()
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles txtb.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        End
+
+    End Sub
+End Class
